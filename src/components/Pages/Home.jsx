34e83c1c94cd475c848243fedc8b1b95d4c89ef9
Alvine <tiekammerciel472@gmail.com>
2024-10-15 12:15:46 +0100

    copie de chat gpt

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+const Home= ()=>{
+
+    console.log("Home")
+    return (
+    <div  >
+        
+        <ul className="menu">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+     <h1>page d'Accueil</h1>
+     </div>
+    )
+}
+export default Home;
